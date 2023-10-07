a7294c25e5000431ba05b87a6fab1591bed767ae
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-10-07 10:53:52 -0300

    chore: Instalando e configurando React 18.2.0

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,31 @@ module.exports = (paths) => ({
     filename: "bundle.js",
   },
   mode: "development",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx|ts|tsx)$/,
         exclude: /(node_modules|bower_components)/,
         include: path.resolve(__dirname, paths.scripts.src),
-        use: "ts-loader",
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        }, //"ts-loader",
+      },
+      //teste
+      {
+        test: /\.tsx?$/, // Esta regra irá corresponder a arquivos .ts e .tsx
+        use: "ts-loader", // Use o ts-loader para processar arquivos TypeScript
+        exclude: /node_modules/,
       },
     ],
   },

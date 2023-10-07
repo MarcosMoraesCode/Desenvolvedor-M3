10e465f7f4199aed8d5557ab08dce0dfc7e28331
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-10-07 18:11:16 -0300

    feat: tipando product Card e mapeando produtos em tela

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,13 +1,39 @@
 import React from "react";
+import { Product } from "../../ts/Product";
 
-const ProductCard = () => {
+const ProductCard = ({
+  color,
+  date,
+  id,
+  image,
+  name,
+  parcelamento,
+  price,
+  size,
+}: Product) => {
+  console.log("aqui", color, date, id, size);
+
+  const productName = name.toUpperCase();
+
+  const productPrice = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(price);
+
+  const productInstallments = new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  }).format(parcelamento[1]);
+
   return (
     <div className="product-card">
-      <img src="../img/img_2.png" />
+      <img src={image} />
       <div className="product-info">
-        <h1 className="product-name">CAMISETA MESCLA</h1>
-        <h1 className="product-price">R$ 28,00</h1>
-        <span className="product-installments">até 3x de R$9,33</span>
+        <h1 className="product-name">{productName}</h1>
+        <h1 className="product-price">{productPrice}</h1>
+        <span className="product-installments">
+          até {parcelamento[0]}x de {productInstallments}
+        </span>
       </div>
       <button onClick={() => {}}>COMPRAR</button>
     </div>
--- src/components/Productsection/ProductSection.tsx
+++ src/components/Productsection/ProductSection.tsx
@@ -1,16 +1,35 @@
 import React from "react";
-import AsideFilters from "../AsideFilters/AsideFilters";
-import ProductCard from "../Product/ProductCard";
+import ProductCard from "../ProductCard/ProductCard";
+import { useProducts } from "../../hooks/useProducts";
+import { Product } from "../../ts/Product";
 
 const ProductSection = () => {
+  const { products } = useProducts();
+
+  let fetchedProducts;
+
+  if (products.length > 0) {
+    console.log(products);
+    fetchedProducts = products.map((product, id) => {
+      return (
+        <ProductCard
+          key={`product-${id}`}
+          name={product.name}
+          color={product.color}
+          date={product.date}
+          id={product.id}
+          image={product.image}
+          parcelamento={product.parcelamento}
+          price={product.price}
+          size={product.size}
+        />
+      );
+    });
+  }
+
   return (
     <section className="products-section">
-      <div className="products-container">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-      </div>
+      <div className="products-container">{fetchedProducts}</div>
       <div className="load-btn-wrapper">
         <button className="load-products-btn" type="button">
           CARREGAR MAIS

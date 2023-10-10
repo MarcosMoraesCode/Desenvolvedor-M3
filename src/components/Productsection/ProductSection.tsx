b3403ff969639309f1044ed290be6f9ad1680639
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-10-10 19:40:41 -0300

    feat: ajustando rexibição responsiva

--- src/components/Productsection/ProductSection.tsx
+++ src/components/Productsection/ProductSection.tsx
@@ -7,65 +7,189 @@ const ProductSection = () => {
   const { products, selectedColors } = useProducts();
   const [productsShownDesktop, setProductShownDesktop] = useState(9); //Define a quantidade de produtos mostrados inicialmente
   const [productsShownMobile, setProductShownMobile] = useState(4);
+  const [productsShownTablet, setProductShownTablet] = useState(6);
+  const [productsShownWideScreen, setProductShownWideScreen] = useState(12);
+
   const onClickHandler = () => {
-    screenSize === "desktop-screen"
-      ? setProductShownDesktop(productsShownDesktop + 4)
-      : setProductShownMobile(productsShownMobile + 4);
+    switch (screenSize) {
+      case "ultrawide-screen":
+        setProductShownWideScreen(productsShownWideScreen + 4);
+        break;
+      case "desktop-screen":
+        setProductShownDesktop(productsShownDesktop + 3);
+        break;
+      case "tablet-screen":
+        setProductShownTablet(productsShownTablet + 3);
+        break;
+      case "mobile-screen":
+        setProductShownMobile(productsShownMobile + 4);
+      default:
+        break;
+    }
+
+    // screenSize === "desktop-screen"
+    //   ? setProductShownDesktop(productsShownDesktop + 4)
+    //   : setProductShownMobile(productsShownMobile + 4);
     //Define quantos produtos a mais serão mostrados ao clicar no botão
   };
 
+  const screenSize = getScreenSize();
+  let productsMessage = "Nenhum produto foi encontrado.";
+
+  const screenSizeTernaryOption = () => {
+    let ternaryOption;
+    switch (screenSize) {
+      case "ultrawide-screen":
+        ternaryOption = productsShownWideScreen >= products.length;
+        break;
+      case "desktop-screen":
+        ternaryOption = productsShownDesktop >= products.length;
+        break;
+      case "tablet-screen":
+        ternaryOption = productsShownTablet >= products.length;
+        break;
+      case "mobile-screen":
+        ternaryOption = productsShownMobile >= products.length;
+        break;
+      default:
+        break;
+    }
+    return ternaryOption;
+  };
+
+  const checkingScreenSize = screenSizeTernaryOption();
+
   let fetchedProducts;
 
-  const screenSize = getScreenSize();
-
   if (products.length > 0) {
-    if (screenSize === "desktop-screen") {
-      fetchedProducts = products
-        .map((product, id) => {
-          return (
-            <ProductCard
-              key={`product-${id}`}
-              name={product.name}
-              color={product.color}
-              date={product.date}
-              id={product.id}
-              image={product.image}
-              parcelamento={product.parcelamento}
-              price={product.price}
-              size={product.size}
-            />
-          );
-        })
-        .filter((_, id) => id < productsShownDesktop);
-    } else {
-      fetchedProducts = products
-        .map((product, id) => {
-          return (
-            <ProductCard
-              key={`product-${id}`}
-              name={product.name}
-              color={product.color}
-              date={product.date}
-              id={product.id}
-              image={product.image}
-              parcelamento={product.parcelamento}
-              price={product.price}
-              size={product.size}
-            />
-          );
-        })
-        .filter((_, id) => id < productsShownMobile);
+    productsMessage = "Todos os produtos foram exibidos.";
+
+    switch (screenSize) {
+      case "ultrawide-screen":
+        fetchedProducts = products
+          .map((product, id) => {
+            return (
+              <ProductCard
+                key={`product-${id}`}
+                name={product.name}
+                color={product.color}
+                date={product.date}
+                id={product.id}
+                image={product.image}
+                parcelamento={product.parcelamento}
+                price={product.price}
+                size={product.size}
+              />
+            );
+          })
+          .filter((_, id) => id < productsShownWideScreen);
+        break;
+      case "desktop-screen":
+        fetchedProducts = products
+          .map((product, id) => {
+            return (
+              <ProductCard
+                key={`product-${id}`}
+                name={product.name}
+                color={product.color}
+                date={product.date}
+                id={product.id}
+                image={product.image}
+                parcelamento={product.parcelamento}
+                price={product.price}
+                size={product.size}
+              />
+            );
+          })
+          .filter((_, id) => id < productsShownDesktop);
+        break;
+      case "tablet-screen":
+        fetchedProducts = products
+          .map((product, id) => {
+            return (
+              <ProductCard
+                key={`product-${id}`}
+                name={product.name}
+                color={product.color}
+                date={product.date}
+                id={product.id}
+                image={product.image}
+                parcelamento={product.parcelamento}
+                price={product.price}
+                size={product.size}
+              />
+            );
+          })
+          .filter((_, id) => id < productsShownTablet);
+        break;
+      case "mobile-screen":
+        fetchedProducts = products
+          .map((product, id) => {
+            return (
+              <ProductCard
+                key={`product-${id}`}
+                name={product.name}
+                color={product.color}
+                date={product.date}
+                id={product.id}
+                image={product.image}
+                parcelamento={product.parcelamento}
+                price={product.price}
+                size={product.size}
+              />
+            );
+          })
+          .filter((_, id) => id < productsShownMobile);
+        break;
+      default:
+        break;
     }
   }
+  //   if (screenSize === "desktop-screen") {
+  //     fetchedProducts = products
+  //       .map((product, id) => {
+  //         return (
+  //           <ProductCard
+  //             key={`product-${id}`}
+  //             name={product.name}
+  //             color={product.color}
+  //             date={product.date}
+  //             id={product.id}
+  //             image={product.image}
+  //             parcelamento={product.parcelamento}
+  //             price={product.price}
+  //             size={product.size}
+  //           />
+  //         );
+  //       })
+  //       .filter((_, id) => id < productsShownDesktop);
+  //   } else {
+  //     fetchedProducts = products
+  //       .map((product, id) => {
+  //         return (
+  //           <ProductCard
+  //             key={`product-${id}`}
+  //             name={product.name}
+  //             color={product.color}
+  //             date={product.date}
+  //             id={product.id}
+  //             image={product.image}
+  //             parcelamento={product.parcelamento}
+  //             price={product.price}
+  //             size={product.size}
+  //           />
+  //         );
+  //       })
+  //       .filter((_, id) => id < productsShownMobile);
+  //   }
+  // }
 
   return (
     <section className="products-section">
       <div className="products-container">{fetchedProducts}</div>
       <div className="load-btn-wrapper">
-        {screenSize === "desktop-screen" ? (
-          productsShownDesktop >= products.length
-        ) : productsShownMobile >= products.length ? (
-          <span className="span-limit">Todos os produtos foram exibidos.</span>
+        {checkingScreenSize ? (
+          <span className="span-limit">{productsMessage}</span>
         ) : (
           <button
             onClick={() => onClickHandler()}
--- src/utils/SizeChecker.ts
+++ src/utils/SizeChecker.ts
@@ -1,9 +1,13 @@
 export function getScreenSize() {
   const width = window.innerWidth;
 
-  if (width < 1024) {
+  if (width < 720) {
     return "mobile-screen";
-  } else {
+  } else if (width >= 720 && width < 1023) {
+    return "tablet-screen";
+  } else if (width >= 1024 && width < 1779) {
     return "desktop-screen";
+  } else if (width >= 1780 && width < 2560) {
+    return "ultrawide-screen";
   }
 }
